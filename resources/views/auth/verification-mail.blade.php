50fe52223571b2b55a2b33081fc788fc8ca9366d
allanjustine <labya200@gmail.com>
2023-12-13 12:25:43 +0800

    Change COmmit

--- resources/views/auth/verification-mail.blade.php
+++ resources/views/auth/verification-mail.blade.php
@@ -2,7 +2,7 @@
 
 <p>You received this email as a result of your registration to our website </p>
 <p>Please click on the verification link to verify your account </p>
-<p>{{ $user->created_at->diffForHumans() }} </p>
+<p>{{ $user->created_at }} </p>
 
 <p>
     <a href="{{ url('/verification/' . $user->id . '/' . $user->remember_token) }}">Click Here</a>
